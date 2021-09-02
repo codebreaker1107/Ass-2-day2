f4f53a035991b2a241d6129ed7cb352ce148150a
Shashank Kumar <89797172+codebreaker1107@users.noreply.github.com>
2021-09-02 12:55:32 +0530

    day 2 assigment submitted

---
+++ sum.java
@@ -0,0 +1,16 @@
+
+/*print sum of two values using command line argument in java*/
+
+class sum
+{
+      public static void main(String ar[])
+      {
+            int x,y,s;
+ 
+            x=Integer.parseInt(ar[0]);
+            y=Integer.parseInt(ar[1]);
+ 
+            s=x+y;
+            System.out.println("sum of " + x + " and " + y +" is " +s);
+      }
+}
